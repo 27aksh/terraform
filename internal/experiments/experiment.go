d9cc67006f146412de3d2ad58ebafcf95782fce5
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2022-02-22 12:03:25 -0500

    Disable preconditions/postconditions
    This functionality is not ready for feedback and may still be removed.
    Disable the experiment for now while we determine next steps.

--- internal/experiments/experiment.go
+++ internal/experiments/experiment.go
@@ -27,7 +27,6 @@ func init() {
 	registerConcludedExperiment(SuppressProviderSensitiveAttrs, "Provider-defined sensitive attributes are now redacted by default, without enabling an experiment.")
 	registerConcludedExperiment(ConfigDrivenMove, "Declarations of moved resource instances using \"moved\" blocks can now be used by default, without enabling an experiment.")
 	registerCurrentExperiment(ModuleVariableOptionalAttrs)
-	registerCurrentExperiment(PreconditionsPostconditions)
 }
 
 // GetCurrent takes an experiment name and returns the experiment value
